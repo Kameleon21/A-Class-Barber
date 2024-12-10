cf7885ca0a6a3679c78dc6db6d5fecc248173685
Kameleon21 <kamilrogozinski29@gmail.com>
2024-12-10 20:45:39 +0000

    contat page added
    update loading time for website and create better navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,27 @@
 import { defineConfig } from 'astro/config';
-
+import compress from 'astro-compress';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [
+    tailwind(),
+    icon(),
+    compress({
+      img: true,
+      svg: true,
+      js: true,
+      css: true,
+    })
+  ],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    },
+    format: ['webp', 'avif'],
+  },
+  build: {
+    inlineStylesheets: 'auto',
+  }
 });
