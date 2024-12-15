2616c7548a636f4ac6e16edebec032a5d3c28a2c
Kameleon21 <kamilrogozinski29@gmail.com>
2024-12-15 13:36:17 +0000

    update contact us section for better main thread optimmaztion

---
+++ src/data/contact.js
@@ -0,0 +1,33 @@
+export const openingHours = [
+    { day: "Monday", hours: "9am - 6pm" },
+    { day: "Tuesday", hours: "9am - 6pm" },
+    { day: "Wednesday", hours: "9am - 6pm" },
+    { day: "Thursday", hours: "9am - 7pm" },
+    { day: "Friday", hours: "9am - 7pm" },
+    { day: "Saturday", hours: "9am - 6pm" },
+    { day: "Sunday", hours: "12am - 5pm" }
+];
+
+export const socialLinks = [
+    {
+        platform: "tiktok",
+        icon: "ic:baseline-tiktok",
+        url: "https://www.tiktok.com/@aclassturkishbarber?lang=en"
+    },
+    {
+        platform: "instagram",
+        icon: "mdi:instagram",
+        url: "https://www.instagram.com/aclass_turkish_barbers/?igsh=MWI2aThydHN2M2F0YQ%3D%3D&utm_source=qr"
+    },
+    {
+        platform: "facebook",
+        icon: "mdi:facebook",
+        url: "https://www.facebook.com/Aclassturkishbarbers/"
+    }
+];
+
+export const contactInfo = {
+    address: "3 Conduit Ln, Waterford, E91 EY05",
+    phone: "083 125 6089",
+    mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2442.0276040446793!2d-7.113044223181753!3d52.2610437719946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4842c7832357be2b%3A0x6a100ea6461b7ac3!2sA-CLASS%20Barbers%20(Turkish%20Barber%20)!5e0!3m2!1sen!2sie!4v1733601698982!5m2!1sen!2sie"
+}; 
