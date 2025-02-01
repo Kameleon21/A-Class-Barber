444ae974d3a89ccf6da3e40dea30bdc902914299
Kamil <kamilrogozinski29@gmail.com>
2025-02-01 19:56:00 +0000

    update spelling and photo for better quality

--- src/data/community.js
+++ src/data/community.js
@@ -1,6 +1,4 @@
 import backToSchool from '../assets/image-3.jpg';
-import shelterDrive from '../assets/image-2.jpg';
-import communityHaircut from '../assets/image-5.jpg';
 import waterfordSoccer from '../assets/image-5.jpg';
 import sponsorship from '../assets/image-21.jpg';
 import southend from '../assets/image-22.jpg';
@@ -8,18 +6,18 @@ import localBoxing from '../assets/image-28.jpg';
 import barberOfTheYear from '../assets/image-29.jpg';
 export const communityInitiatives = [
     {
-        title: "Leadng by example",
-        description: "At A-Class Barber's we hold ourselves to the highest standards of professionalism and customer service. We are proud to be a part of the community and we are committed to supporting local initiatives. It was an honour and a privilege to recive the barber of the year award in 2024. This award is a testament to the hard work and dedication of our team and the support of our customers. We are proud to be a part of the community and we are committed to supporting local initiatives.",
+        title: "Leading by example",
+        description: "At A-Class Barber's we hold ourselves to the highest standards of professionalism and customer service. We are proud to be a part of the community and we are committed to supporting local initiatives. It was an honour and a privilege to receive the barber of the year award in 2024. This award is a testament to the hard work and dedication of our team and the support of our customers. We are proud to be a part of the community and we are committed to supporting local initiatives.",
         image: barberOfTheYear,
         imageAlt: "Community haircut event",
-        imagePosition: "left"
+        imagePosition: "left" // This will determine the image position (left/right)
     },
     {
         title: "Sponsoring the Drama Club",
         description: "At A Class Barber, we believe a thriving community is nurtured through the arts, where creativity and self-expression bring people together. That’s why we’re proud to sponsor the Drama Club, a local theatrical group dedicated to inspiring young performers and seasoned actors alike. Our contribution helps the club secure costumes, props, and rehearsal spaces—ensuring that each production brings the community closer, sparks meaningful conversations, and nurtures a passion for the performing arts. Through this sponsorship, we’re not only investing in quality entertainment but in the dreams and voices of our neighbors who step onto the stage to bring stories to life.",
         image: backToSchool,
         imageAlt: "Back to school haircut event",
-        imagePosition: "right" // This will determine the image position (left/right)
+        imagePosition: "right"
     },
     {
         title: "Sponsoring Waterford Football Club",
@@ -30,13 +28,13 @@ export const communityInitiatives = [
     },
     {
         title: "Waterford Rose",
-        description: "This image highlights A-Class Barber's sponsorship of Anne for the Rose of Waterford. Anne, a former trainee at the barber shop, is now supported by her old workplace as she takes on the prestigious role. This sponsorship underscores the strong communnity bonds and the barbe's shop's commitment to uplifiting local talent. it reflects their dedication to the community and the importance of supporting local talent.",
+        description: "This image highlights A-Class Barber's sponsorship of Anne for the Rose of Waterford. Anne, a former trainee at the barber shop, is now supported by her old workplace as she takes on the prestigious role. This sponsorship underscores the strong community bonds and the barber's shop's commitment to uplifting local talent. It reflects their dedication to the community and the importance of supporting local talent.",
         image: sponsorship,
         imageAlt: "Sponsorship",
         imagePosition: "right"
     },
     {
-        title: "Sounthend FC sponsorship",
+        title: "Southend FC sponsorship",
         description: "This image captures yet another powerful sponsorship by A-Class Barber's, this time supporting a young boy's football club. The sponsorship not only provides essential resources for the young athletes but also strengthens the community ties.",
         image: southend,
         imageAlt: "southend",
@@ -44,7 +42,7 @@ export const communityInitiatives = [
     },
     {
         title: "Sponsoring local boxing club",
-        description: "A-Class Barber's is proudly sponsoring Paul's boxing club and the talented boxer Shamie. The collaboration between the barber shop and the boxing club exemplifies the positive influence local business can have on individual athleats and sports organisations, driving success and strengthing community bonds.",
+        description: "A-Class Barber's is proudly sponsoring Paul's boxing club and the talented boxer Shamie. The collaboration between the barber shop and the boxing club exemplifies the positive influence local business can have on individual athletes and sports organisations, driving success and strengthening community bonds.",
         image: localBoxing,
         imageAlt: "local boxing club",
         imagePosition: "right"
